b1ef8528cf9a07a5b3e6b39a53995fa7a4da2f38
gokturkDev <gokturkbuyuktuna00@gmail.com>
2021-12-11 12:01:36 +0300

    chore: created loss wın methods

--- go/go-reminders/go-learner_03-dir/player.go
+++ go/go-reminders/go-learner_03-dir/player.go
@@ -1,15 +1,24 @@
 package player
 
 type Playerer interface {
-	FlipCoin()
+	Won()
+	Loss()
 }
 
 type Player struct {
 	money int
+	wins  int
+	loss  int
 }
 
 type Coin struct {
 	heads, tails bool
 }
+
+func (ply Player) Won(amount int) {
+	ply.money -= amount
+}
+
+func (ply Player) Lost(amount int) { //this is how to create a method, very interesting architecture
 
 }
