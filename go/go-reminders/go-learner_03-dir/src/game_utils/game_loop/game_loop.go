e3cf8c10ab703dd12547435f0ab3b5ec433fb691
gokturkDev <gokturkbuyuktuna00@gmail.com>
2021-12-11 14:41:01 +0300

    fıx: go packagıng ıs really really weırd

--- go/go-reminders/go-learner_03-dir/src/game_utils/game_loop/game_loop.go
+++ go/go-reminders/go-learner_03-dir/src/game_utils/game_loop/game_loop.go
@@ -1,11 +1,66 @@
-package game_loop
+package main
 
-import "game_utils/coin"
+import (
+	"fmt"
+	"game_utils/player"
+	"os"
+	"os/exec"
 
-func StartGameLoop() {
+	"github.com/inancgumus/screen"
+)
 
+func main() {
+	fmt.Print("Welcome to the GOLang coin flipper-uh!\nPress enter to continue:")
+	// disable input buffering
+	exec.Command("stty", "-F", "/dev/tty", "cbreak", "min", "1").Run()
+	// do not display entered characters on the screen
+	exec.Command("stty", "-F", "/dev/tty", "-echo").Run()
+
+	var b []byte = make([]byte, 1)
+	os.Stdin.Read(b)
+	StartGameLoop()
 }
 
-func gameLoop(player1, player2 coin.Coin) {
+func StartGameLoop() {
+	human_player := player.New()
+	ai_player := player.New()
+	gameLoop(&human_player, &ai_player)
+}
+
+func gameLoop(human_player, ai_player *player.Player) {
+	isPlayerTurn := true
+	for { //this is the while of GO
+		screen.Clear()
+		fmt.Printf("Your coins left: %d", human_player.GetMoney())
+		fmt.Printf("AI coins left: %d", ai_player.GetMoney())
+		if isPlayerTurn {
+			playerTurn(human_player)
+			isPlayerTurn = false
+		} else {
+			aiTurn(ai_player)
+		}
+	}
+}
+
+func playerTurn(player *player.Player) {
+	fmt.Println("It is your turn!")
+	for {
+		if *getBet() > player.GetMoney() {
+			fmt.Println("You don't have that much!")
+			continue
+		} else {
+			break
+		}
+	}
+}
+
+func getBet() *int {
+	var bet int
+	fmt.Println("Enter the amount you want to bet:")
+	fmt.Scan(&bet)
+	return &bet
+}
+
+func aiTurn(player *player.Player) {
 
 }
