45aed0841047bb7997c102372a4b46d56d1075ff
gokturkDev <gokturkbuyuktuna00@gmail.com>
2021-12-11 14:11:17 +0300

    chore: player getters

--- go/go-reminders/go-learner_03-dir/src/game_utils/player/player.go
+++ go/go-reminders/go-learner_03-dir/src/game_utils/player/player.go
@@ -13,10 +13,24 @@ type Player struct {
 
 func (ply *Player) Won(amount int) {
 	ply.money += amount
+	ply.wins += 1
 }
 
 func (ply *Player) Lost(amount int) { //this is how to create a method, very interesting architecture
 	ply.money -= amount
+	ply.loss += 1
+}
+
+func (ply *Player) GetMoney() int {
+	return ply.money
+}
+
+func (ply *Player) GetWinCount() int {
+	return ply.wins
+}
+
+func (ply *Player) GetLossCount() int {
+	return ply.loss
 }
 
 func New() Player {
