f13a31db6cfadd3c78220be183114bb52062ddc6
gokturkDev <gokturkbuyuktuna00@gmail.com>
2021-12-11 13:03:52 +0300

    chore: method changes object dırectly

--- go/go-reminders/go-learner_03-dir/src/game_utils/player/player.go
+++ go/go-reminders/go-learner_03-dir/src/game_utils/player/player.go
@@ -11,10 +11,18 @@ type Player struct {
 	loss  int
 }
 
-func (ply Player) Won(amount int) {
+func (ply *Player) Won(amount int) {
 	ply.money += amount
 }
 
-func (ply Player) Lost(amount int) { //this is how to create a method, very interesting architecture
+func (ply *Player) Lost(amount int) { //this is how to create a method, very interesting architecture
 	ply.money -= amount
+}
+
+func New() Player {
+	return Player{
+		money: 1000,
+		wins:  0,
+		loss:  0,
+	}
 }
