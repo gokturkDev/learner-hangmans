aa24c335795d1134cfaa664c19df961318cb2287
gokturkDev <gokturkbuyuktuna00@gmail.com>
2021-12-11 11:51:21 +0300

    chore: removed flıp coın logıc from player class

--- go/go-reminders/go-learner_03-dir/player.go
+++ go/go-reminders/go-learner_03-dir/player.go
@@ -1,10 +1,5 @@
 package player
 
-import (
-	"math/rand"
-	"time"
-)
-
 type Playerer interface {
 	FlipCoin()
 }
@@ -17,12 +12,4 @@ type Coin struct {
 	heads, tails bool
 }
 
-func (ply Player) FlipCoin(amount int) Coin {
-	rand.NewSource(time.Now().Unix())
-	randNum := rand.Intn(1)
-	ply.money -= amount
-	if randNum < 1 {
-		return Coin{heads: true}
-	}
-	return Coin{tails: true}
 }
