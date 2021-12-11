d210ffe37ca75e608a3e5b629dd128629713b6df
gokturkDev <gokturkbuyuktuna00@gmail.com>
2021-12-11 15:16:16 +0300

    delete: no flıp coın

--- go/go-reminders/go-learner_03-dir/src/game_utils/coin/coin.go
+++ go/go-reminders/go-learner_03-dir/src/game_utils/coin/coin.go
@@ -1,19 +1,9 @@
 package coin
 
-import (
-	"math/rand"
-	"time"
-)
-
 type Coin struct {
 	Heads, Tails bool
 }
 
-func flipACoin() Coin {
-	rand.NewSource(time.Now().Unix())
-	randNum := rand.Intn(1)
-	if randNum < 1 {
 		return Coin{Heads: true}
 	}
-	return Coin{Tails: true}
 }
