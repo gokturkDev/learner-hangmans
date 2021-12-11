78cba074e58d939bfbaf934b11b5452a2c68a8fa
gokturkDev <gokturkbuyuktuna00@gmail.com>
2021-12-11 14:38:35 +0300

    Revert "chore: aı and player turn"
    This reverts commit 837898a3a71f89a6e7840d2b93215c904174b32d.

--- go/go-reminders/go-learner_03-dir/src/game_utils/game_loop/game_loop.go
+++ go/go-reminders/go-learner_03-dir/src/game_utils/game_loop/game_loop.go
@@ -1,37 +1,11 @@
 package game_loop
 
-import (
-	"fmt"
-	"game_utils/player"
-
-	"github.com/inancgumus/screen"
-)
+import "game_utils/coin"
 
 func StartGameLoop() {
-	human_player := player.New()
-	ai_player := player.New()
-	screen.Clear()
-	gameLoop(&human_player, &ai_player)
-}
-
-func gameLoop(human_player, ai_player *player.Player) {
-	isPlayerTurn := true
-	for { //this is the while of GO
-		fmt.Printf("Your coins left: %d", human_player.GetMoney())
-		fmt.Printf("AI coins left: %d", ai_player.GetMoney())
-		if isPlayerTurn {
-			playerTurn(human_player)
-			isPlayerTurn = false
-		} else {
-			aiTurn(ai_player)
-		}
-	}
-}
-
-func playerTurn(player *player.Player) {
 
 }
 
-func aiTurn(player *player.Player) {
+func gameLoop(player1, player2 coin.Coin) {
 
 }
